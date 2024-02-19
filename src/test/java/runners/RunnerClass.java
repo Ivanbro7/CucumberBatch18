c928855535660415c3a01e736974bb0db26baddb
Ivanbro7 <ivanbro7@gmail.com>
2024-02-19 17:35:06 -0500

    v7

--- src/test/java/runners/RunnerClass.java
+++ src/test/java/runners/RunnerClass.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         dryRun = false,
         //tags = "@sprint1 and @sprint4"
         //tags = "@sprint1 or @sprint4"
-        tags = "@sprint3",
+        tags = "@sprint1",
         //from
         plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json"
                 //rerun plugin generates the txt file of all failed test cases
